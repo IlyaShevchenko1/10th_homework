29293f319a728c3708049a7c0ac40a3393b89ed6
Shevchenko Ilya <6blK@mail.ru>
2024-01-17 19:37:24 +0300

    Work is done

--- main.py
+++ main.py
@@ -9,3 +9,18 @@
 # data = pd.DataFrame({'whoAmI':lst})
 # data.head()
 
+import pandas as pd
+import random
+
+lst = ['robot'] * 10
+lst += ['human'] * 10
+print(lst)
+print()
+random.shuffle(lst)
+data = pd.DataFrame({'whoAmI':lst})
+data.loc[data['whoAmI'] == 'robot', 'robot_group'] = '1'  #Добавляем столбец robot_group
+data.loc[data['whoAmI'] != 'robot', 'robot_group'] = '0'
+data.loc[data['whoAmI'] == 'human', 'human_group'] = '1'  #Добавляем столбец human_group
+data.loc[data['whoAmI'] != 'human', 'human_group'] = '0'
+data.head()
+print(data)
